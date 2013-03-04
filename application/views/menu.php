39d7a392363fa5c817718361a203605f5f3812ff
Stef Louwers <s.louwers@treva.nl>
2013-03-04 22:40:54 +0100

    menu actievatie gefixed

--- application/views/menu.php
+++ application/views/menu.php
@@ -5,10 +5,10 @@ echo Navigation::lists(
 			array(Navigation::HEADER, 'Algemeen', false, false, null),
 			array('Home', URL::to_route('home'), Request::route()->is('home')),
 			array('Dagboek', URL::to_route('dagboek'), Request::route()->is('dagboek')),
-			array('Vogels', URL::to_route('vogels'), Request::route()->is('vogels')),
-			array('Soorten', URL::to_route('soorten'), Request::route()->is('soorten')),
+			array('Vogels', URL::to_route('vogels'), Request::route()->is('vogels') || Request::route()->is('vogelDetail')),
+			array('Soorten', URL::to_route('soorten'), Request::route()->is('soorten') || Request::route()->is('soortDetail')),
 // 			array('Taken', URL::to_route('taken'), Request::route()->is('taken')),
-			array('Gebruikers', URL::to_route('gebruikers'), Request::route()->is('gebruikers')),
+			array('Gebruikers', URL::to_route('gebruikers'), Request::route()->is('gebruikers') || Request::route()->is('gebruikerDetail')),
 			array(Navigation::DIVIDER),
 			array('Logout', URL::to_route('logout'), Request::route()->is('logout')),
 		)
