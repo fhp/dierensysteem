8aac2283c0c0b719df109bbcda8de654e59be8f7
Stef Louwers <s.louwers@treva.nl>
2013-05-19 18:46:52 +0200

    datum tonen bij bewerken aanwezigheid

--- application/views/agenda/aanwezigheid.blade.php
+++ application/views/agenda/aanwezigheid.blade.php
@@ -5,6 +5,8 @@
 <h1>Aanwezigheid van {{ $aanwezigheid->gebruiker->naam }}</h1>
 
 {{ Form::horizontal_open() }}
+<? $datum = new DateTime($aanwezigheid->datum); ?>
+{{ Form::control_group(Form::label('datum', 'Datum'), "<div style=\"padding-top: 5px;\">" . $datum->format("d-m-Y") . "</div>") }}
 {{ Form::control_group(Form::label('actief', 'Aanmelding actief'), Form::checkbox('actief', '1', $aanwezigheid->actief)) }}
 {{ Form::control_group(Form::label('opmerkingen', 'Opmerkingen'), Form::text('opmerkingen', $aanwezigheid->opmerkingen)) }}
 {{ Form::actions(array(Button::primary_submit('Opslaan'))) }}
